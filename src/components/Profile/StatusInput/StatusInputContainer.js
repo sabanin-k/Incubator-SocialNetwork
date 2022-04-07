90e139870353236da12d6e30dd873cb34859d4b8
Konstantin Sabanin <sabanin_k@mail.ru>
2022-04-07 21:49:09 +0300

    StatusInput form Class to Func. Edit UserProfile. Some minor refactoring.

--- src/components/Profile/StatusInput/StatusInputContainer.js
+++ src/components/Profile/StatusInput/StatusInputContainer.js
@@ -1,25 +1,25 @@
 import { connect } from "react-redux"
 import { compose } from "redux"
 import { getAuthUserId } from "../../State/Selectors/authSelector"
-import { getStatus } from "../../State/Selectors/userProfileSelector"
-import { updateStatusThunk, getStatusThunk } from "../../State/userProfileReducer"
+import { getStatus, getUserId } from "../../State/Selectors/userProfileSelector"
+import { updateStatusThunk } from "../../State/userProfileReducer"
 import StatusInput from "./StatusInput"
 
 const StatusInputContainer = (props) => {
     return <StatusInput
         status={props.status}
-        getStatus={props.getStatusThunk}
+        userId={props.userId}
+        authUserId={props.authUserId}
         updateStatus={props.updateStatusThunk}
-        userId={props.userId} // откуда здесь props.userId я вообще хз, в пропсах его быть не должно
-        authUserId={props.authUserId}/>
+    />
 }
 
 const mapStateToProps = (state) => ({
     status: getStatus(state),
-    // userId: state.userProfilePage.userProfile.userId,
+    userId: getUserId(state),
     authUserId: getAuthUserId(state)
 })
 
 export default compose(
-    connect(mapStateToProps, { updateStatusThunk , getStatusThunk })
+    connect(mapStateToProps, { updateStatusThunk })
 )(StatusInputContainer)
